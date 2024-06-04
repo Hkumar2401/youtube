39df6a0e7e0bd79cf2b6714a99a5e98c93233361
Hemant Kumar <hemantk240103@gmail.com>
2024-06-04 18:54:20 +0530

    make search suggestion work using rapid api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,40 @@ import { useDispatch, useSelector } from "react-redux";
 import YoutubeLogo from "../images/youtube-icon.png";
 import { toggleDarkMode, toggleSidebar } from "../utils/slices/configSlice";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  RAPID_OPTIONS,
+  RAPID_SEARCH_SUGGESTIONS_API,
+} from "../utils/constatnts";
 
 const Header = () => {
   const [searchText, setSearchText] = useState("");
-  
+  const [searhSuggestions, setSearchSuggestions] = useState([]);
+  const [showSearchSuggestions, setShowSearchSuggestions] = useState(false);
+
   const dispatch = useDispatch();
 
   const isDarkMode = useSelector((store) => store.config.isDarkMode);
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchText]);
+
+  const getSearchSuggestions = async () => {
+    try {
+      const res = await fetch(
+        RAPID_SEARCH_SUGGESTIONS_API + searchText,
+        RAPID_OPTIONS
+      );
+      const data = await res.json();
+      setSearchSuggestions(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="flex justify-between items-center text-black dark:text-white dark:bg-black py-2">
@@ -23,7 +49,7 @@ const Header = () => {
           <div className="w-4 h-[3px] bg-black dark:bg-white rounded-3xl"></div>
         </div>
 
-        <Link to={'/'}>
+        <Link to={"/"}>
           <div className="ml-6 flex items-center justify-center cursor-pointer">
             <img className="w-[32px]" src={YoutubeLogo} alt="" />
             <h1 className="ml-1 font-bold text-[20px] text-black dark:text-white">
@@ -34,17 +60,37 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="flex justify-center  items-center">
-        <input
-          placeholder="Search"
-          className="text-black dark:text-white bg-white  dark:bg-black w-[500px] h-8 p-5 rounded-s-3xl focus:outline focus:outline-blue-400 focus:-outline-offset-1 border-[1px] border-solid border-[#cccccc] dark:border-zinc-700"
-          type="text"
-          value={searchText}
-          onChange={(e)=> setSearchText(e.target.value)}
-        />
-        <button className="bg-[#f0f0f0] hover:bg-[#e5e5e5]  dark:bg-[#222222] dark:hover:bg-[#111111] active:bg-[#c1c1c1] dark:active:bg-[#555555] p-2 w-20 rounded-e-3xl border-[1px] border-s-0 border-solid border-[$cccccc] dark:border-zinc-700">
-          <i className="fa-solid fa-magnifying-glass text-black dark:text-white"></i>
-        </button>
+      <div className="flex justify-center flex-col relative">
+        <div className="flex justify-center items-center">
+          <input
+            placeholder="Search"
+            className="text-black dark:text-white bg-white  dark:bg-black w-[500px] h-8 p-5 rounded-s-3xl focus:outline focus:outline-blue-400 focus:-outline-offset-1 border-[1px] border-solid border-[#cccccc] dark:border-zinc-700"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            onFocus={() => setShowSearchSuggestions(true)}
+            onBlur={() => setShowSearchSuggestions(false)}
+          />
+          <button className="bg-[#f0f0f0] hover:bg-[#e5e5e5]  dark:bg-[#222222] dark:hover:bg-[#111111] active:bg-[#c1c1c1] dark:active:bg-[#555555] p-2 w-20 rounded-e-3xl border-[1px] border-s-0 border-solid border-[$cccccc] dark:border-zinc-700">
+            <i className="fa-solid fa-magnifying-glass text-black dark:text-white"></i>
+          </button>
+        </div>
+        <div
+          className={`bg-white dark:bg-black border-[1px] border-[#f0f0f0] dark:border-[#222222] text-black dark:text-white w-[500px] absolute top-12 py-3 z-50 rounded-lg shadow-2xl ${
+            showSearchSuggestions ? "block" : "hidden"
+          }`}
+        >
+          {searhSuggestions?.map((suggestion) => {
+            return (
+              <Link to="" key={suggestion}>
+                <div className="flex px-5 py-1 items-center hover:bg-[#f0f0f0] dark:hover:bg-[#222222]">
+                  <i className="fa-solid fa-magnifying-glass text-black dark:text-white mr-4 text-sm"></i>
+                  <p className="">{suggestion}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
 
       <div className="mr-3 flex justify-center items-center">
